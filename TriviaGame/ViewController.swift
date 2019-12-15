2f6e12fa5415500a6078ace940511a3b509a4e18
Anan Sadiya <anansadiya@gmail.com>
2019-12-15 20:56:02 +0200

    Base project

---
+++ TriviaGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TriviaGame
+//
+//  Created by Anan Sadiya on 15/12/2019.
+//  Copyright © 2019 Anan Sadiya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
